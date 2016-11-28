7245487b2f34a9a27773bf006e14735224745f28
ChrisHoyer <choyer.ch@googlemail.com>
2016-11-28 22:15:07 +0100

    Update call.bat
    projectname now without file extension

--- call.bat
+++ call.bat
@@ -2,20 +2,21 @@
 :: clear config file
 break > vs2015
 
-:: clear quotes
-
-
 :: rewrite config file
 ( 
-	echo @INPUT 				= 	default
+	echo @INCLUDE 				= 	default
+
+	echo # Add project name and number out of vs2015
 	
-	echo PROJECT_NAME   		= 	%~3
-	echo PROJECT_NUMBER         = 	
+	echo PROJECT_NAME   		= %~n3
+	
+	echo # using enviroment variables for input and output	
 	
 	echo INPUT                  =   %~1	
 	echo OUTPUT_DIRECTORY       =   %~2doxygen\
 	
 ) > vs2015
+
 
 
 doxygen vs2015
